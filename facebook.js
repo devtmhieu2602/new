73a85aee5299b8fe30678e44a205cf4b5af50007
devtmhieu2602 <64523166+devtmhieu2602@users.noreply.github.com>
2020-05-04 13:58:26 +0700

    Update facebook.js

--- facebook.js
+++ facebook.js
@@ -47,9 +47,9 @@ function Facebook(accessToken, groupId)
         formPublish.append('message',
             `# TIN TỨC BUỔI TRƯA ${date}\n` +
             '\n' +
-            '## thông tin\n' +
+            'thông tin\n' +
             '* Bài viết được tự động đăng lúc 12 giờ trưa hàng ngày\n' +
-            '* Đây là post tự động được tạo bởi Lerni và lấy dữ liệu từ VnExpress qua RSS');
+            '* Đây là post tự động lấy dữ liệu từ VnExpress qua RSS');
         formPublish.append('formatting', 'MARKDOWN');
 
         for (let i = 0; i < images.length ; i++) {
